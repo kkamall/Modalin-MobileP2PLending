ee5c0dd1b1f992fc5164343012034325a49589d7
kkamall <kamalrobbani88@gmail.com>
2023-05-23 12:40:27 +0700

    Profile Borrower

---
+++ source_code/lib/add_pinjaman.dart
@@ -0,0 +1,345 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  MyAppState createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  final textEditController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  // Controller buat text
+  final TextEditingController _judulVideoController = TextEditingController();
+  final TextEditingController _linkYoutubeController = TextEditingController();
+  final TextEditingController _jumlahPinjamanController =
+      TextEditingController();
+  final TextEditingController _returnInvestasiController =
+      TextEditingController();
+  final TextEditingController _waktuPinjamanController =
+      TextEditingController();
+
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {
+      // Form is valid, perform desired action
+      String judulVideo = _judulVideoController.text;
+      String linkYoutube = _linkYoutubeController.text;
+      String jumlahPinjaman = _jumlahPinjamanController.text;
+      String returnInvestasi = _returnInvestasiController.text;
+      String waktuPinjaman = _waktuPinjamanController.text;
+      // Process the input data or perform other operations
+      print('Submitted Judul Video: $judulVideo');
+      print('Submitted Link Youtube: $linkYoutube');
+      print('Submitted Jumlah Pinjaman: $jumlahPinjaman');
+      print('Submitted Return Investasi: $returnInvestasi');
+      print('Submitted Waktu Pinjaman: $waktuPinjaman');
+    }
+  }
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    textEditController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hello App',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color(0xFF3D2645),
+                Color(0xFF000000)
+              ], // Replace with your desired colors
+              begin: Alignment.topCenter, // Define the gradient starting point
+              end: Alignment.bottomCenter, // Define the gradient ending point
+            ),
+          ),
+          child: Container(
+            margin: EdgeInsets.fromLTRB(26, 20, 26, 20),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "MODALIN",
+                      style: GoogleFonts.outfit(
+                        color: Color(0xFFFFFFFF),
+                        fontSize: 32,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.arrow_back_outlined),
+                      color: Colors.white,
+                      iconSize: 38,
+                    ),
+                  ],
+                ),
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      // Menampilkan Logo
+                      Container(
+                        margin: EdgeInsets.only(bottom: 25.0),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(14.0),
+                          child: Image.asset(
+                            'assets/images/logo.png',
+                            width: 105,
+                            height: 105,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      // Form penambahan UMKM
+                      Form(
+                        key: _formKey,
+                        child: Column(
+                          children: [
+                            // Field Nama UMKM
+                            Container(
+                              height: 32,
+                              width: 224,
+                              child: TextFormField(
+                                controller: _judulVideoController,
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Masukkan judul video';
+                                  }
+                                  return null;
+                                },
+                                style: GoogleFonts.rubik(
+                                  fontWeight: FontWeight.w600,
+                                  color: Color(0xFFFFFFFF),
+                                  fontSize: 13,
+                                ),
+                                decoration: InputDecoration(
+                                  filled: true,
+                                  fillColor: Color(0x7FF0EFF4),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(26),
+                                  ),
+                                  labelText: 'Judul Video',
+                                  labelStyle: GoogleFonts.rubik(
+                                    fontWeight: FontWeight.w400,
+                                    color: Color(0xFFFFFFFF),
+                                    fontSize: 13,
+                                  ),
+                                  contentPadding: EdgeInsets.symmetric(
+                                    vertical: 10.0,
+                                    horizontal: 18.0,
+                                  ), // Adjust vertical value padding
+                                  floatingLabelBehavior: FloatingLabelBehavior
+                                      .never, // Remove label animation
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 20.0),
+                            // Field Tahun Berdiri UMKM
+                            Container(
+                              height: 32,
+                              width: 224,
+                              child: TextFormField(
+                                controller: _linkYoutubeController,
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Masukkan link video youtube';
+                                  }
+                                  return null;
+                                },
+                                style: GoogleFonts.rubik(
+                                  fontWeight: FontWeight.w600,
+                                  color: Color(0xFFFFFFFF),
+                                  fontSize: 13,
+                                ),
+                                decoration: InputDecoration(
+                                  filled: true,
+                                  fillColor: Color(0x7FF0EFF4),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(26),
+                                  ),
+                                  labelText: 'Link Video Youtube',
+                                  labelStyle: GoogleFonts.rubik(
+                                    fontWeight: FontWeight.w400,
+                                    color: Color(0xFFFFFFFF),
+                                    fontSize: 13,
+                                  ),
+                                  contentPadding: EdgeInsets.symmetric(
+                                    vertical: 10.0,
+                                    horizontal: 18.0,
+                                  ), // Adjust vertical value padding
+                                  floatingLabelBehavior: FloatingLabelBehavior
+                                      .never, // Remove label animation
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 20.0),
+                            // Field Lokasi UMKM
+                            Container(
+                              height: 32,
+                              width: 224,
+                              child: TextFormField(
+                                controller: _jumlahPinjamanController,
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Masukkan jumlah pinjaman';
+                                  }
+                                  return null;
+                                },
+                                style: GoogleFonts.rubik(
+                                  fontWeight: FontWeight.w600,
+                                  color: Color(0xFFFFFFFF),
+                                  fontSize: 13,
+                                ),
+                                decoration: InputDecoration(
+                                  filled: true,
+                                  fillColor: Color(0x7FF0EFF4),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(26),
+                                  ),
+                                  labelText: 'Jumlah Pinjaman',
+                                  labelStyle: GoogleFonts.rubik(
+                                    fontWeight: FontWeight.w400,
+                                    color: Color(0xFFFFFFFF),
+                                    fontSize: 13,
+                                  ),
+                                  contentPadding: EdgeInsets.symmetric(
+                                    vertical: 10.0,
+                                    horizontal: 18.0,
+                                  ), // Adjust vertical value padding
+                                  floatingLabelBehavior: FloatingLabelBehavior
+                                      .never, // Remove label animation
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 20.0),
+                            // Field Return Investasi
+                            Container(
+                              height: 32,
+                              width: 224,
+                              child: TextFormField(
+                                controller: _returnInvestasiController,
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Masukkan return investasi';
+                                  }
+                                  return null;
+                                },
+                                style: GoogleFonts.rubik(
+                                  fontWeight: FontWeight.w600,
+                                  color: Color(0xFFFFFFFF),
+                                  fontSize: 13,
+                                ),
+                                decoration: InputDecoration(
+                                  filled: true,
+                                  fillColor: Color(0x7FF0EFF4),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(26),
+                                  ),
+                                  labelText: 'Return Investasi "%"',
+                                  labelStyle: GoogleFonts.rubik(
+                                    fontWeight: FontWeight.w400,
+                                    color: Color(0xFFFFFFFF),
+                                    fontSize: 13,
+                                  ),
+                                  contentPadding: EdgeInsets.symmetric(
+                                    vertical: 10.0,
+                                    horizontal: 18.0,
+                                  ), // Adjust vertical value padding
+                                  floatingLabelBehavior: FloatingLabelBehavior
+                                      .never, // Remove label animation
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 20.0),
+                            // Field Waktu Pinjaman
+                            Container(
+                              height: 32,
+                              width: 224,
+                              child: TextFormField(
+                                controller: _waktuPinjamanController,
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Masukkan waktu pinjaman';
+                                  }
+                                  return null;
+                                },
+                                style: GoogleFonts.rubik(
+                                  fontWeight: FontWeight.w600,
+                                  color: Color(0xFFFFFFFF),
+                                  fontSize: 13,
+                                ),
+                                decoration: InputDecoration(
+                                  filled: true,
+                                  fillColor: Color(0x7FF0EFF4),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(26),
+                                  ),
+                                  labelText: 'Waktu Pinjaman',
+                                  labelStyle: GoogleFonts.rubik(
+                                    fontWeight: FontWeight.w400,
+                                    color: Color(0xFFFFFFFF),
+                                    fontSize: 13,
+                                  ),
+                                  contentPadding: EdgeInsets.symmetric(
+                                    vertical: 10.0,
+                                    horizontal: 18.0,
+                                  ), // Adjust vertical value padding
+                                  floatingLabelBehavior: FloatingLabelBehavior
+                                      .never, // Remove label animation
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 25.0),
+                            SizedBox(
+                              width: 151,
+                              height: 27,
+                              child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  primary: Color(0xFF832161),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(17.0),
+                                  ),
+                                ),
+                                onPressed: _submitForm,
+                                child: Text(
+                                  'Submit',
+                                  style: GoogleFonts.rubik(
+                                    fontWeight: FontWeight.w500,
+                                    color: Color(0xFFFFFFFF),
+                                    fontSize: 13,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
